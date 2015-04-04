63c0d15ba731f546519411af08c6221413bad3af
Adam <adam@xyzdigital.com>
2015-04-04 10:03:35 -0700

    Update tests

--- tests.py
+++ tests.py
@@ -1,63 +1,149 @@
 import unittest
+import random
+import socket
 import threading
 import time
 from Request import *
 from Response import *
-from UDPClient import UDPClient
 from NodeCircle import *
+
+node_circle = None
 
 class TestKVStore(unittest.TestCase):
     PORT = 11111
 
-    @classmethod
-    def setUpClass(self):
-        self.node_circle = NodeCircle()
-        self.client = UDPClient(TestKVStore.PORT)
-        self.client.socket.setblocking(False)
-        self.client.socket.settimeout(0.5)
-
-    def wait_for_pending_requests(self):
-        while(len(self.client.pending_requests) > 0):
-            pass
-
     def discover_down_nodes(self):
-        request = PingRequest()
-        for node in self.node_circle.nodes:
-            self.client.send_request(request, node.get_addr(), None, self.discover_failed)
-        self.wait_for_pending_requests()
+        global node_circle
 
-    def discover_failed(self, request):
-        print "Node down: " + str(request.dest_addr)
-        self.node_circle.set_node_online_with_addr(request.dest_addr, False)
+        request = PingRequest()
+        for node in node_circle.get_online_nodes():
+            resp = self.send_request(request, node)
+            if resp == None:
+                print "Node down: " + str(node)
+                node.online = False
 
     def setUp(self):
-        self.client_thread = threading.Thread(target=self.client.run)
-        self.client_thread.start()
-        self.discover_down_nodes()
-        self.assertNotEqual(len(self.node_circle.get_online_nodes()), 0)
-        self.test_node = self.node_circle.nodes[0]
+        global node_circle
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(("", 0))
+        self.socket.setblocking(True)
+        self.socket.settimeout(0.5)
+        self.last_uid_time = time.time()
+        self.used_uids = []
+        self.num_requests = 0;
+
+        if node_circle is None:
+            node_circle = NodeCircle()
+            self.discover_down_nodes()
+
+        self.assertNotEqual(len(node_circle.get_online_nodes()), 0)
+
+        self.socket.settimeout(5)
+        self.online_nodes = node_circle.get_online_nodes()
+        self.test_node = random.choice(self.online_nodes)
 
     def tearDown(self):
-        self.client_thread.join()
+        self.socket.close()
+        pass
 
     def test_get_nonexistent_key(self):
-        resp = self.get("test_get_nonexistent_key", self.test_node)
+        resp = self.get("test_get_nonexistent_key")
         self.assert_nonexistent_key(resp)
 
-    def get(self, key, node):
-        return self.send_request(GetRequest(key), node)
+    def test_put_and_remove(self):
+        key = "test_put"
+        value = "test_put"
+        self.assert_successful_request(self.put(key, value))
+        self.assert_get_value(self.get(key), value)
+        self.assert_successful_request(self.remove(key))
 
-    def put(self, key, value, node):
-        return self.send_request(PutRequest(key, value), node)
+    def test_cached_response(self):
+        key = "test_cached_response"
+        uid = self.get_uid()
 
-    def remove(self, key, node):
-        return self.send_request(PutRequest(key, value), node)
+        self.assert_nonexistent_key(self.get(key, None, uid))
+        # check for same response
+        self.assert_nonexistent_key(self.put(key, "", None, uid))
+        # Make sure the put didn't happen
+        self.assert_nonexistent_key(self.get(key))
 
-    def send_request(self, request, node):
-        self.response = None
-        self.client.send_request(request, node.get_addr(), self.get_response)
-        self.wait_for_pending_requests()
-        return self.response
+    def test_replace(self):
+        key = "test_replace"
+        val1 = "test_replace1"
+        val2 = "test_replace2"
+        self.put(key, val1)
+        self.put(key, val2)
+        self.assert_get_value(self.get(key), val2)
+
+    def test_remove_nonexistent(self):
+        self.assert_nonexistent_key(self.remove("test_remove_nonexistent"))
+
+    def test_concurrent_put(self):
+        uid1 = self.get_uid()
+        uid2 = self.get_uid()
+        val1 = "val1"
+        val2 = "val2"
+        uid2.timestamp += 1
+
+        key = "test_concurrent_put"
+        self.put(key, val1, None, uid1)
+        self.put(key, val2, None, uid2)
+        self.put(key, val1, None, uid1)
+
+        self.assert_get_value(self.get(key), val2)
+
+    def test_put_one_get_all(self):
+        key = "test_put_one_get_all"
+        val = "test_put_one_get_all"
+        self.put(key, val)
+        for node in self.online_nodes:
+            self.assert_get_value(self.get(key, node), val)
+        self.remove(key)
+
+    # def test_shutdown(self):
+    #     self.assert_successful_request(self.shutdown())
+    #     self.assert_no_response(self.get(""))
+
+    def get_uid(self):
+        uid = None
+
+        if time.time() - self.last_uid_time > 0.001:
+            self.used_uids = []
+
+        while uid is None or uid in self.used_uids:
+            uid = UID(("127.0.0.1", 0))
+
+        self.last_uid_time = time.time()
+        self.used_uids.append(uid)
+        return uid
+
+    def get(self, key, node = None, uid = None):
+        return self.send_request(GetRequest(key), node, uid)
+
+    def put(self, key, value, node = None, uid = None):
+        return self.send_request(PutRequest(key, value), node, uid)
+
+    def remove(self, key, node = None, uid = None):
+        return self.send_request(RemoveRequest(key), node, uid)
+
+    def shutdown(self, node = None):
+        return self.send_request(ShutdownRequest(), node)
+
+    def send_request(self, request, node = None, uid = None):
+        if node is None:
+            node = self.test_node
+
+        if uid is None:
+            uid = UID(("127.0.0.1", 0))
+
+        self.num_requests += 1
+        self.socket.sendto(uid.get_bytes() + request.get_bytes(), node.get_addr())
+        try:
+            data, addr = self.socket.recvfrom(20000)
+            self.assertEqual(uid.get_bytes(), data[:16])
+            return data[16:]
+        except:
+            return None
 
     def get_response(self, message):
         self.response = message
@@ -71,9 +157,16 @@ class TestKVStore(unittest.TestCase):
     def assert_nonexistent_key(self, response):
         self.assert_response_with_code(response, Response.NON_EXISTENT)
 
+    def assert_get_value(self, response, expected_value):
+        self.assert_successful_request(response)
+        self.assertEqual(response[3:], expected_value)
+
+    def assert_successful_request(self, response):
+        self.assert_response_with_code(response, Response.SUCCESS)
+
     def assert_response_with_code(self, response, expected_response_code):
         self.assert_response(response)
-        self.assertEqual(response.payload[0], expected_response_code)
+        self.assertEqual(response[0], expected_response_code)
 
 if __name__ == '__main__':
     unittest.main()
