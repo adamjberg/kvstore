020d76459381fbfc5e08ef0aff7dd98b68cc86b2
Adam <adam@xyzdigital.com>
2015-04-05 10:22:48 -0700

    Prepare monitoring server thread

---
+++ MonitorServerThread.py
@@ -0,0 +1,57 @@
+from threading import Thread
+import json
+import socket
+import time
+
+class MonitorServerThread(Thread):
+    SERVER_ADDR = ("54.68.197.12", 41170)
+    MONITOR_DELAY_SECONDS = 10
+
+    def __init__(self, node_circle, kvStore):
+        Thread.__init__(self)
+        self.daemon = True
+        self.node_circle = node_circle
+        self.kvStore = kvStore
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(("", 41171))
+        self.hostname = socket.gethostname()
+
+
+    def run(self):
+        while True:
+            self.socket.sendto(self.get_data(), MonitorServerThread.SERVER_ADDR)
+            time.sleep(MonitorServerThread.MONITOR_DELAY_SECONDS)
+
+    def get_data(self):
+        data = {}
+        data["hostname"] = self.hostname
+        data["systemUptime"] = 0
+        data["spaceAvailable"] = 200
+        data["averageLoads"] = ""
+        data["serviceUptime"] = ""
+        data["loc"] = {}
+        data["logs"] = {}
+        data["kvstore"] = self.get_successors_data()
+        data["index"] = self.node_circle.my_node.location
+
+        return json.JSONEncoder().encode(data)
+
+    def get_successors_data(self):
+        data = []
+        for node in self.node_circle.all_nodes:
+            node_data = {}
+            node_data["hostname"] = node.ip
+            node_data["port"] = node.port
+            node_data["location"] = node.location
+            node_data["status"] = node.online
+            node_data["lastUpdateDate"] = "NOW"
+            node_data["spaceAvailable"] = 0
+            node_data["kvstore"] = {}
+
+            if node == self.node_circle.my_node:
+                node_data["kvstore"] = self.kvStore.kv_dict
+                node_data["spaceAvailable"] = self.kvStore.space_available
+
+            data.append(node_data)
+
+        return data
