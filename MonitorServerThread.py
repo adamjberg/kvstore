d082d4e6994a896fc5c1ec577346842c31e90620
Adam <adam@xyzdigital.com>
2015-04-07 13:09:24 -0700

    Add more monitoring info

--- MonitorServerThread.py
+++ MonitorServerThread.py
@@ -1,12 +1,19 @@
 from threading import Thread
+from datetime import datetime, timedelta
 import simplejson as json
 import socket
+import subprocess
 import time
 import urllib2
 
+def check_output(args):
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    out, err = p.communicate()
+    return out
+
 class MonitorServerThread(Thread):
     SERVER_ADDR = ("54.68.197.12", 41170)
-    MONITOR_DELAY_SECONDS = 15
+    MONITOR_DELAY_SECONDS = 10
 
     def __init__(self, node_circle, kvStore):
         Thread.__init__(self)
@@ -16,29 +23,52 @@ class MonitorServerThread(Thread):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(("", 0))
         self.hostname = socket.gethostname()
-        self.loc = urllib2.urlopen("http://ip-api.com/json/").read()
+        try:
+            self.loc = urllib2.urlopen("http://ip-api.com/json/", timeout=2).read()
+        except:
+            self.loc = {"status" :"fail"}
+
+        self.service_start_time = datetime.now()
 
     def run(self):
         while True:
             try:
                 self.socket.sendto(self.get_data(), MonitorServerThread.SERVER_ADDR)
-                time.sleep(MonitorServerThread.MONITOR_DELAY_SECONDS)
             except:
                 pass
+            time.sleep(MonitorServerThread.MONITOR_DELAY_SECONDS)
 
     def get_data(self):
         data = {}
         data["hostname"] = self.hostname
-        data["systemUptime"] = 0
-        data["spaceAvailable"] = 300
-        data["averageLoads"] = ""
-        data["serviceUptime"] = ""
+        data["systemUptime"] = ""
+        data["spaceAvailable"] = self.get_available_space()
+        data["averageLoads"] = self.get_average_load()
+        data["serviceUptime"] = self.get_service_uptime()
         data["loc"] = self.loc
         data["logs"] = {}
         data["kvstore"] = self.get_successors_data()
         data["index"] = self.node_circle.my_node.location
 
         return json.JSONEncoder().encode(data)
+
+    def get_system_uptime(self):
+        system_start_time = datetime.strptime(check_output(["uptime", "-s"]), "%Y-%m-%d %H:%M:%S\n")
+        return str(datetime.now() - system_start_time)
+
+    def get_service_uptime(self):
+        return str(datetime.now() - self.service_start_time)
+
+    def get_average_load(self):
+        uptime_string = check_output(["uptime"])
+        return uptime_string[-17:]
+
+    def get_available_space(self):
+        df_string = check_output(["df", "-h"])
+        df_array = df_string.split("\n")
+        for line in df_array:
+            if line[-1] == "/":
+                return line[28:32]
 
     def get_successors_data(self):
         data = []
@@ -48,7 +78,7 @@ class MonitorServerThread(Thread):
             node_data["port"] = node.port
             node_data["location"] = node.location
             node_data["status"] = node.online
-            node_data["lastUpdateDate"] = "NOW"
+            node_data["lastUpdateDate"] = ""
             node_data["spaceAvailable"] = 0
             node_data["kvstore"] = {}
 
